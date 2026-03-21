93d849db0c1255efa048529842adc806a567be35
Sundram1343 <sundramsharma2901@gmail.com>
2026-03-21 23:53:04 +0530

    working on urgent page

--- src/components/Urgent.jsx
+++ src/components/Urgent.jsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View,ScrollView, Pressable } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context' 
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React from 'react';
 const Urgent = () => {
   return (
     <SafeAreaView>
@@ -10,19 +10,21 @@ const Urgent = () => {
           <Text style={styles.ViewAllText}>View All</Text>
         </Pressable>
       </View>
-      <ScrollView style={styles.urgentBloodneed}>
-        <Text style={styles.BloodNamr}>O-</Text>
-        
-        <Text>O- Negative Needed</Text>
-        <View style={styles.address}>
-          <Text >City General Hospiatl</Text>
-          <Text>.</Text>
-          <Text>Unit</Text>
-        </View>     
-        <View style={styles.DonateNow}>
-          <Pressable>
-            <Text style={styles.DonateNowText}>Donate Now</Text>
-          </Pressable>
+      <ScrollView>
+        <View style={styles.urgentBloodneed}>
+          <Text style={styles.BloodName}>O-</Text>
+
+          <Text style={styles.NameNeeded}>O- Negative Needed</Text>
+          <View style={styles.address}>
+            <Text>City General Hospiatl</Text>
+            <Text>.</Text>
+            <Text>Unit</Text>
+          </View>
+          <View style={styles.DonateNow}>
+            <Pressable>
+              <Text style={styles.DonateNowText}>Donate Now</Text>
+            </Pressable>
+          </View>
         </View>
       </ScrollView>
       <View style={styles.DonationCenterConatiner}>
@@ -38,25 +40,43 @@ const Urgent = () => {
         </Pressable>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Urgent
+export default Urgent;
 
 const styles = StyleSheet.create({
-  NeededBlood:{
+  NeededBlood: {
+    flexDirection: 'row',
+    gap: 145,
+    marginLeft: 5,
+  },
+  Name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  ViewAllText: {
+    fontSize: 15,
+    marginTop: 5,
+    color: '#132845',
+  },
+  urgentBloodneed: {
+    marginLeft: 7,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    marginRight:2.5,
+    height:200,
+  },
+  BloodName:{
+    fontSize:35,
+    color:'#e63746',
+  },
+  NameNeeded:{
+    fontWeight:'bold',
+    fontSize:20,
+  },
+  address:{
     flexDirection:'row',
-    gap:145,
-    marginLeft:5
-  },
-  Name:{
-    fontSize:24,
-    fontWeight:'bold'
-  },
-  ViewAllText:{
-    fontSize:15,
-    marginTop:5,
-    color:'#132845',
-    
+    gap:5
   }
-})
+});
